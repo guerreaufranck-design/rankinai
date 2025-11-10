a16d30853d75d51dcf383669b06043a2af7ad710
guerreaufranck-design <guerreau.franck@gmail.com>
2025-11-10 19:10:33 +0000

    fix: Correct syntax in webhook routes - remove template literals bug

--- app/routes/webhooks.shop.redact.tsx
+++ app/routes/webhooks.shop.redact.tsx
@@ -3,6 +3,6 @@ import { authenticate } from "~/shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { shop, topic } = await authenticate.webhook(request);
-  console.log(`Received ${topic} webhook for ${shop}`);
+  console.log("Received " + topic + " webhook for " + shop);
   return new Response();
 };
